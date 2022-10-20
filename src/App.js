32b5b17f192a988daf8a206d05f86bde7a1bca18
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-20 11:25:49 +0700

    added appButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./reset.scss";
 import AppText from "./components/app-text/AppText";
+import AppButton from "./components/app-button/AppButton";
 
 function App() {
   return (
     <div className="app">
-      <AppText text="hihihihihihihi" fontSize={64} fontWeight={500}/>
+      <AppText text="Гидрокситриптофан (5-htp)" fontSize={16} fontWeight={500}/>
+      <AppButton text={'Добавить'} />
     </div>
   );
 }
---
+++ src/components/app-button/AppButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AppText from "../app-text/AppText";
+import "./index.scss";
+
+const AppButton = ({isActive, text}) => {
+    return (
+        <button
+            className={
+                `app-button ${isActive ? 'app-button_active'  : ''}`
+            }
+        >
+            <AppText
+                fontSize={16}
+                text={text}
+            />
+        </button>
+    );
+};
+
+export default AppButton;
