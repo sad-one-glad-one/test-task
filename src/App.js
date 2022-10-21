d84a9b2ad8f4a7dabdd8c06f0d9d049adc369c5c
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-21 20:34:10 +0700

    added appCollapse

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import AppText from "./components/app-text/AppText";
 import AppButton from "./components/app-button/AppButton";
 import { AppIcon } from "./components/app-icon/AppIcon";
 import AppTabs from "./components/app-tabs/AppTabs";
+import AppCollapse from "./components/app-collapse/AppCollapse";
 
 function App() {
 	const tabs = [{text: "по времени приёма", index: 1}, {text: "по биодобавке", index: 2}]
 	const [activeTab, setActiveTab] = useState(1)
 
   return (
-    <div className="app">
+    <div className="app" style={{display:'grid', gridTemplateColumns: '1fr', gridRowGap: '24px'}}>
       <AppText text="Гидрокситриптофан (5-htp)" fontSize={16} fontWeight={500}/>
-      <AppButton text={'Добавить'} />
+      <div>
+				<AppButton text={'Добавить'} />
+			</div>
 			<AppTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+			<AppCollapse />
       <div style={{display:'grid', gridTemplateColumns: "repeat(6,1fr)"}}>
         <AppIcon name="icon-anti-age" width="64" height="64" color="teal" opacity="0.7" />
         <AppIcon name="icon-antioxydants" width="64" height="64" color="teal" opacity="0.5" />
---
+++ src/components/app-collapse/AppCollapse.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { AppIcon } from '../app-icon/AppIcon';
+import AppText from '../app-text/AppText';
+import "./index.scss"
+
+const AppCollapse = () => {
+	const [isOpen, setIsOpen] = useState(false)
+	const handleEvent = () => {
+		setIsOpen(!isOpen)
+	}
+
+	return (
+		<div className="app-collapse" onClick={()=>handleEvent()}>
+			<div className="app-collapse__header">
+				<div className="app-collapse__header-inner app-collapse__header-inner_left">
+					<div className="app-collapse__header-chevron">
+						<AppIcon
+							name="icon-chevron-up" 
+							width="24" 
+							height="24" 
+							color="rgba(0, 0, 0, 0.87)"
+						/>
+					</div>
+					<AppText 
+						text="Ежедневно в 19:00" 
+						lineHeight={24} 
+						fontWeight={500} 
+					/>
+				</div>
+				<div className="app-collapse__header-inner app-collapse__header-inner_right">
+					<AppText 
+						text="5 шт: Гидрокситриптофан (5-htp), Цинк + В6, Мультиви..." 
+						fontSize={14}
+						lineHeight={24} 
+					/>
+					<AppIcon 
+						name="icon-trash" 
+						width="24" 
+						height="24" 
+						color="#A61911"
+					/>
+				</div>
+			</div>
+			<div className="app-collapse__body">
+			</div>
+		</div>
+	);
+};
+
+export default AppCollapse;
