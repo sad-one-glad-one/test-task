64b8cc3c2cc07e4547063be620b1e73e0296fd54
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-22 12:10:11 +0700

    added class

--- src/components/app-text/AppText.jsx
+++ src/components/app-text/AppText.jsx
@@ -3,16 +3,17 @@ import React from "react";
 const AppText = ({text, fontSize, lineHeight, fontWeight, color}) => {
 
     return (
-        <p 
-            style={{
-                fontSize : fontSize? `${fontSize}px` : '16px',
-                lineHeight : lineHeight? `${lineHeight}px` : '20px',
-                fontWeight : fontWeight? fontWeight : 400,
-                color: color ? color : 'rgba(0, 0, 0, 0.87)'
-            }}
-        >
-            {text}
-        </p>
+      <p 
+				className="app-text"
+        style={{
+            fontSize : fontSize? `${fontSize}px` : '16px',
+            lineHeight : lineHeight? `${lineHeight}px` : '20px',
+            fontWeight : fontWeight? fontWeight : 400,
+            color: color ? color : 'rgba(0, 0, 0, 0.87)'
+        }}
+      >
+        {text}
+      </p>
     );
 };
 
