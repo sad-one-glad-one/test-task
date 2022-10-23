85cf1996d3f0b1a7964ab1b07a2db3ecfad56833
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-23 23:22:11 +0700

    finished right sidebar

--- src/components/app-sidebar/AppSidebar.jsx
+++ src/components/app-sidebar/AppSidebar.jsx
@@ -1,35 +1,95 @@
 import React, { useState } from 'react';
 import { AppIcon } from '../app-icon/AppIcon';
+import AppSelect from '../app-select/AppSelect';
+import AppText from '../app-text/AppText';
 import "./index.scss"
 import Logo from "./logo.png"
 
-const AppSidebar = ({children}) => {
+const AppSidebar = ({isRight, children}) => {
 	const [isShort, setIsShort] = useState(false)
+	const toggleSidebar = () => {
+		setIsShort(!isShort)
+	}
+
+	let appSidebar = isRight && isShort ? "app-sidebar-right app-sidebar-right_short" 
+												: isRight && !isShort ? "app-sidebar-right app-sidebar-right_long" 
+													: !isRight && isShort ? "app-sidebar app-sidebar_short"
+														: "app-sidebar app-sidebar_long"
+
+	const selectOptions = [
+		{name: '4 недели'},
+		{name: '3 недели'},
+		{name: '2 недели'},
+	]
+	const [selectedOption, setSelectedOption] = useState('4 недели')
 
 	return (
-		<div className={`app-sidebar ${isShort ? 'app-sidebar_short' : 'app-sidebar_long'}`}>
-			<div className="app-sidebar__head">
-				<div className="app-sidebar__head-inner">
-					<div className="app-sidebar__head-logo">
-						<img src={Logo} alt="Logo" draggable={false} />
+		<aside className={appSidebar}>
+			{!isRight ?
+				<div className="app-sidebar__head">
+					<div className="app-sidebar__head-inner">
+						<div className="app-sidebar__head-logo">
+							<img src={Logo} alt="Logo" draggable={false} />
+						</div>
+						<button
+							className="app-sidebar__head-btn align-center"
+							onClick={() => toggleSidebar()}
+						>
+							<AppIcon 
+								name="icon-menu"
+								width="24"
+								height="24"
+								color="rgba(0, 0, 0, 0.87)"
+							/>
+						</button>
 					</div>
-					<button
-						className="app-sidebar__head-btn align-center"
-						onClick={() => setIsShort(!isShort)}
+				</div>
+			:
+				<div className="app-sidebar-right__head">
+					<button 
+						className="app-sidebar-right__head-btn"
+						onClick={() => toggleSidebar()}
 					>
-						<AppIcon 
-							name="icon-menu"
-							width="24"
-							height="24"
-							color="rgba(0, 0, 0, 0.87)"
+						<div className={`app-sidebar-right__head-btn-icon align-center ${isShort && "app-sidebar-right__head-btn-icon_flipped"}`}>
+							<AppIcon 
+									name="icon-chevron-up"
+									width="24"
+									height="24"
+									color="rgba(0, 0, 0, 0.87)"
+								/>
+						</div>
+						<AppText 
+							text="Мой курс приёма" 
+							fontSize={22}
+							lineHeight={26}
+							fontWeight={500}
 						/>
 					</button>
+					{!isShort && 
+						<div className="app-sidebar-right__head-params">
+							<AppSelect 
+								options={selectOptions} 
+								selectedOption={selectedOption} 
+								setSelectedOption={setSelectedOption} 
+							/>
+							<button
+								className="head-params__btn"
+								onClick={() => alert("Свернуть все")}
+							>
+								<AppText
+									text="Свернуть все"
+									lineHeight={24}
+									color="#2662C9"
+								/>
+							</button>
+						</div>
+					}
 				</div>
-			</div>
+			}
 			<div className="app-sidebar__body">
 				{children}
 			</div>
-		</div>
+		</aside>
 	);
 };
 
