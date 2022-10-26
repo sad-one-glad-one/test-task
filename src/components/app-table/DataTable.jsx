5ed937cfd2ccb2eb0a014469c97c5b06a11a18fe
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-26 23:13:55 +0700

    added table sort & styles

--- src/components/app-table/DataTable.jsx
+++ src/components/app-table/DataTable.jsx
@@ -1,38 +1,85 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { sortData } from '../../store/DataReducer';
 import AppButton from '../app-button/AppButton';
 import { AppIcon } from '../app-icon/AppIcon';
 import AppText from '../app-text/AppText';
 import "./index.scss";
 
 const DataTable = () => {
+	const dispatch = useDispatch()
 	const data = useSelector(state => state.fromApi.data)
+	let sortingArray = [...data]
+	let [sortableTitle, setSortableTitle] = useState([
+		{key: "GoodsCommercialName", direction: "ascending", isActive: false},
+		{key: "CurrentPrices", direction: "ascending", isActive: false}
+	])
+
+	const handleSort = (field) => {
+		let paramDirection = field.direction === "ascending" ? 1 : -1
+		sortingArray.sort((a, b) => (
+			a[field.key] < b[field.key] ? -paramDirection
+				: a[field.key] > b[field.key] ? paramDirection
+					: 0
+			)
+		)
+		dispatch(sortData(sortingArray))
+		toggleSortDirection(field)
+	}
+
+	const toggleSortDirection = (sortedObj) => {
+		let copySortedArray = Object.assign([], sortableTitle)
+		copySortedArray.find(item => {
+			item.isActive = false
+			if (item === sortedObj) {
+				item.isActive = true
+				item.direction === "ascending" ?
+					item.direction = "descending"
+					: item.direction = "ascending"
+			}
+		})
+		setSortableTitle([...copySortedArray])
+	}
+
+	const isAccendingNameClass = sortableTitle[0].direction === "ascending" ?
+		"table-th__sortable_asc"
+		: "table-th__sortable_desc"
+	const isAccendingPriceClass = sortableTitle[1].direction === "ascending" ?
+		"table-th__sortable_asc" 
+		: "table-th__sortable_desc"
 
 	return (
 		<div className="table-wrap">
 			<table className="table">
 				<thead>
 					<tr>
-						<th className="table-th__sortable">
+						<th
+							className={`table-th__sortable ${isAccendingNameClass} `}
+							onClick={() => handleSort(sortableTitle[0])}
+						>
 							<AppText text="Биодобавка" lineHeight={19} />
 							<AppIcon 
+								className="app-icon"
 								name="icon-sort" 
 								width="24" 
 								height="24" 
-								color="rgba(0, 0, 0, 0.16)"
+								color={sortableTitle[0].isActive ? "#2662C9" : "rgba(0, 0, 0, 0.16)"}
 							/>
 						</th>
 						<th>
 							<AppText text="Описание" lineHeight={19} />
 						</th>
-						<th className="table-th__sortable">
+						<th
+							className={`table-th__sortable ${isAccendingPriceClass} `}
+							onClick={() => handleSort(sortableTitle[1])}
+						>
 							<AppText text="Цена за шт., ₽" lineHeight={19} />
 							<AppIcon 
 								name="icon-sort" 
 								width="24" 
 								height="24" 
-								color="rgba(0, 0, 0, 0.16)"
-							/>		
+								color={sortableTitle[1].isActive ? "#2662C9" : "rgba(0, 0, 0, 0.16)"}
+							/>
 						</th>
 						<th></th>
 					</tr>
@@ -42,16 +89,16 @@ const DataTable = () => {
 						<tr key={item?.GoodsCommercialName}>
 							<td className="table__product">
 								<div className="table__product-img align-center">
-									<img src={item.Picture} />
+									<img src={item?.Picture} />
 								</div>
 								<AppText
-									text={item.GoodsCommercialName}
+									text={item?.GoodsCommercialName}
 									lineHeight={24}
 								/>
 							</td>
 							<td className="table__description">
 								<AppText 
-									text={item.CommercialDescription}
+									text={item?.CommercialDescription}
 									fontSize={14}
 									lineHeight={18}
 									color={"rgba(0, 0, 0, 0.64)"}
@@ -59,7 +106,7 @@ const DataTable = () => {
 							</td>
 							<td className="table__price">
 								<AppText
-									text={`${item.CurrentPrices} ₽`}
+									text={`${item?.CurrentPrices} ₽`}
 									lineHeight={24}
 									fontWeight={700}
 								/>
--- src/store/DataReducer.js
+++ src/store/DataReducer.js
@@ -7,6 +7,7 @@ const defaultState = {
 const FETCH_DATA = "FETCH_DATA"
 const SET_ERRORS = "SET_ERRORS"
 const TOGGLE_IS_LOADING = "TOGGLE_IS_LOADING"
+const SORT_DATA = "SORT_DATA"
 
 export const dataReducer = (state = defaultState, action) => {
 	switch (action.type) {
@@ -16,6 +17,8 @@ export const dataReducer = (state = defaultState, action) => {
 			return {...state, errors: [...state.errors, ...action.payload]}
 		case TOGGLE_IS_LOADING:
 			return {...state, isLoading: state.action}
+		case SORT_DATA:
+			return {...state, data: [...action.payload]}
 		
 		default:
 			return state
@@ -25,3 +28,4 @@ export const dataReducer = (state = defaultState, action) => {
 export const fetchDataAction = (payload) => ({type: FETCH_DATA, payload})
 export const setErrorsAction = (payload) => ({type: SET_ERRORS, payload})
 export const toggleIsLoadingAction = (payload) => ({type: TOGGLE_IS_LOADING, payload})
+export const sortData = (payload) => ({type: SORT_DATA, payload})
