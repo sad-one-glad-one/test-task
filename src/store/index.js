cc060696888f8c5dc0dcec70183245f3a3ded596
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-28 00:34:10 +0700

    imported new reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
-import { dataReducer } from "./DataReducer"
+import { apiDataReducer } from "./ApiDataReducer"
+import { localDataReducer } from "./LocalDataReducer"
 import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk"
 
 const rootReducer = combineReducers({
-	fromApi: dataReducer
+	fromApi: apiDataReducer,
+	fromStore: localDataReducer
 })
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
