672dba3af29d8eb24da35e8e85863e9d5c6ba8bf
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-28 11:10:23 +0700

    added modal reducer

---
+++ src/store/ModalReducer.js
@@ -0,0 +1,24 @@
+const defaultState = {
+	modalState: {
+		isOpen: false,
+		modalData: {}
+	}
+}
+
+const OPEN_MODAL = "OPEN_MODAL"
+const CLOSE_MODAL = "CLOSE_MODAL"
+
+export const modalReducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case OPEN_MODAL:
+			return {...state, modalState: {isOpen: true, modalData: action.payload}}
+		case CLOSE_MODAL:
+			return {...state, modalState: {isOpen: false, modalData: {}}}
+		
+		default:
+			return state
+	}
+}
+
+export const openModalAction = (payload) => ({type: OPEN_MODAL, payload})
+export const closeModalAction = (payload) => ({type: CLOSE_MODAL, payload})
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import { apiDataReducer } from "./ApiDataReducer"
 import { localDataReducer } from "./LocalDataReducer"
+import { modalReducer } from "./ModalReducer"
 import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk"
 
 const rootReducer = combineReducers({
 	fromApi: apiDataReducer,
-	fromStore: localDataReducer
+	fromStore: localDataReducer,
+	modal: modalReducer
 })
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
