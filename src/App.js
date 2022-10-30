ffd4be8d4f64be7d638ee632af4d1e244fe3eecc
sad-one-glad-one <deuslovult17@gmail.com>
2022-10-30 22:57:07 +0700

    added login btn

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const apiData = useSelector((state) => state.fromApi.data)
   const basketData = useSelector((state) => state.basket.basketData)
   const dispatch = useDispatch()
-
+  const [isAuth, setIsAuth] = useState(false)
   const [navbar] = useState([
     { name: "Anti-age", icon: "icon-anti-age" },
     { name: "Антистресс", icon: "icon-antistress" },
@@ -71,61 +71,106 @@ function App() {
           }}
         >
           <AppSidebar isShort={isNavbarShort} setIsShort={setIsNavbarShort}>
-            <nav
-              className={`navbar-body ${
-                isNavbarShort ? "navbar-body_short" : "navbar-body_long"
-              }`}
-            >
-              <header
-                className="navbar-body__title navbar-body__title_active"
-                onClick={() => restoreArr()}
+            <div className="navbar-body__wrap">
+              <nav
+                className={`navbar-body ${
+                  isNavbarShort ? "navbar-body_short" : "navbar-body_long"
+                }`}
               >
-                <AppText text="Каталог" fontWeight={500} lineHeight={16} />
-              </header>
-              <ul className={`navbar-body__list `}>
-                {navbar.map((item) => (
-                  <li
-                    className="navbar-body__list-item"
-                    key={item.name}
-                    onClick={() => sortArr(item.name)}
+                <header
+                  className="navbar-body__title navbar-body__title_active"
+                  onClick={() => restoreArr()}
+                >
+                  <AppText text="Каталог" fontWeight={500} lineHeight={16} />
+                </header>
+                <ul className={`navbar-body__list `}>
+                  {navbar.map((item) => (
+                    <li
+                      className="navbar-body__list-item"
+                      key={item.name}
+                      onClick={() => sortArr(item.name)}
+                    >
+                      {!isNavbarShort ? (
+                        <AppText text={item.name} />
+                      ) : (
+                        <div className="icon-wrap align-center">
+                          {/* TODO: обдумать что делать с hover'ом и иконками */}
+                          <AppIcon
+                            name={item.icon}
+                            width="24"
+                            height="24"
+                            color="rgba(0, 0, 0, 0.87)"
+                          />
+                        </div>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+                {!isNavbarShort && (
+                  <div>
+                    <header className="navbar-body__title">
+                      <AppText
+                        text="Нутрицилоги"
+                        fontWeight={500}
+                        lineHeight={16}
+                      />
+                    </header>
+                    <ul className="navbar-body__list"></ul>
+                    <header className="navbar-body__title">
+                      <AppText
+                        text="Документы"
+                        fontWeight={500}
+                        lineHeight={16}
+                      />
+                    </header>
+                    <ul className="navbar-body__list"></ul>
+                  </div>
+                )}
+              </nav>
+              <div
+                className={`navbar-body__auth ${
+                  isNavbarShort ? "navbar-body_short" : "navbar-body_long"
+                }`}
+              >
+                {isAuth ? (
+                  <button
+                    className="navbar-body__auth-btn navbar-body__auth-btn_logined align-center"
+                    onClick={() => setIsAuth(false)}
                   >
-                    {!isNavbarShort ? (
-                      <AppText text={item.name} />
-                    ) : (
-                      <div className="icon-wrap align-center">
-                        {/* TODO: обдумать что делать с hover'ом и иконками */}
-                        <AppIcon
-                          name={item.icon}
-                          width="24"
-                          height="24"
-                          color="rgba(0, 0, 0, 0.87)"
-                        />
-                      </div>
-                    )}
-                  </li>
-                ))}
-              </ul>
-              {!isNavbarShort && (
-                <div>
-                  <header className="navbar-body__title">
-                    <AppText
-                      text="Нутрицилоги"
-                      fontWeight={500}
-                      lineHeight={16}
+                    <div className="navbar-body__auth-btn-logo align-center">
+                      <AppText
+                        text="ЮШ"
+                        fontSize={12}
+                        fontWeight={700}
+                        color="#2662C9"
+                      />
+                    </div>
+                    <div className="text-wrap">
+                      <AppText
+                        text="Юлия Шмелёва"
+                        fontSize={14}
+                        lineHeight={16}
+                      />
+                    </div>
+                  </button>
+                ) : (
+                  <button
+                    className="navbar-body__auth-btn align-center"
+                    onClick={() => setIsAuth(true)}
+                  >
+                    <AppIcon
+                      name="icon-log-in"
+                      width="24"
+                      height="24"
+                      color="rgba(0, 0, 0, 0.87)"
                     />
-                  </header>
-                  <ul className="navbar-body__list"></ul>
-                  <header className="navbar-body__title">
-                    <AppText
-                      text="Документы"
-                      fontWeight={500}
-                      lineHeight={16}
-                    />
-                  </header>
-                  <ul className="navbar-body__list"></ul>
-                </div>
-              )}
-            </nav>
+                    <div className="text-wrap">
+                      <AppText text="Войти" fontSize={14} lineHeight={16} />
+                    </div>
+                  </button>
+                )}
+              </div>
+            </div>
           </AppSidebar>
           <DataTable />
           <AppSidebar
