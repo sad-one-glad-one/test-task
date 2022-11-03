d56efa03813bd3f37d230d094d25e91368848b9b
sad-one-glad-one <deuslovult17@gmail.com>
2022-11-04 03:12:33 +0700

    added adaptive

--- src/components/app-sidebar/AppSidebar.jsx
+++ src/components/app-sidebar/AppSidebar.jsx
@@ -1,92 +1,96 @@
-import React, { useState } from 'react';
-import { AppIcon } from '../app-icon/AppIcon';
-import AppSelect from '../app-select/AppSelect';
-import AppText from '../app-text/AppText';
+import React, { useState } from "react"
+import { AppIcon } from "../app-icon/AppIcon"
+import AppSelect from "../app-select/AppSelect"
+import AppText from "../app-text/AppText"
 import "./index.scss"
 import Logo from "./logo.png"
 
 const AppSidebar = ({ isShort, setIsShort, isRight, children }) => {
-	const toggleSidebar = () => {
-		setIsShort(!isShort)
-	}
-	let appSidebar = isRight && isShort ? "app-sidebar-right app-sidebar-right_short" 
-												: isRight && !isShort ? "app-sidebar-right app-sidebar-right_long" 
-													: !isRight && isShort ? "app-sidebar app-sidebar_short"
-														: "app-sidebar app-sidebar_long"
+  const toggleSidebar = () => {
+    setIsShort(!isShort)
+  }
+  let appSidebar =
+    isRight && isShort
+      ? "app-sidebar-right app-sidebar-right_short"
+      : isRight && !isShort
+      ? "app-sidebar-right app-sidebar-right_long"
+      : !isRight && isShort
+      ? "app-sidebar app-sidebar_short"
+      : "app-sidebar app-sidebar_long"
 
-	const selectOptions = [
-		{name: '4 недели'},
-		{name: '3 недели'},
-		{name: '2 недели'},
-	]
-	const [selectedOption, setSelectedOption] = useState('4 недели')
+  const selectOptions = [
+    { name: "4 недели" },
+    { name: "3 недели" },
+    { name: "2 недели" },
+  ]
+  const [selectedOption, setSelectedOption] = useState("4 недели")
 
-	return (
-		<aside className={appSidebar}>
-			{!isRight ?
-				<div className="app-sidebar__head">
-					<div className="app-sidebar__head-inner">
-						<div className="app-sidebar__head-logo">
-							<img src={Logo} alt="Logo" draggable={false} />
-						</div>
-						<button
-							className="app-sidebar__head-btn align-center"
-							onClick={() => toggleSidebar()}
-						>
-							<AppIcon 
-								name="icon-menu"
-								width="24"
-								height="24"
-								color="rgba(0, 0, 0, 0.87)"
-							/>
-						</button>
-					</div>
-				</div>
-			:
-				<div className="app-sidebar-right__head">
-					<button 
-						className="app-sidebar-right__head-btn"
-						onClick={() => toggleSidebar()}
-					>
-						<div className={`app-sidebar-right__head-btn-icon align-center ${isShort && "app-sidebar-right__head-btn-icon_flipped"}`}>
-							<AppIcon 
-									name="icon-chevron-up"
-									width="24"
-									height="24"
-									color="rgba(0, 0, 0, 0.87)"
-								/>
-						</div>
-						<AppText 
-							text="Мой курс приёма" 
-							fontSize={22}
-							lineHeight={26}
-							fontWeight={500}
-						/>
-					</button>
-					<div className={`app-sidebar-right__head-params ${isShort ? "display-none" : ""}`}>
-						<AppSelect 
-							options={selectOptions} 
-							selectedOption={selectedOption} 
-							setSelectedOption={setSelectedOption} 
-						/>
-						<button
-							className="head-params__btn"
-							onClick={() => alert("Свернуть все")}
-						>
-							<AppText
-								text="Свернуть все"
-								lineHeight={24}
-								color="#2662C9"
-							/>
-						</button>
-						</div>
-				</div>
-			}
-			<div className="app-sidebar__body">
-				{children}
-			</div>
-		</aside>
-	);
-};
+  return (
+    <aside className={appSidebar}>
+      {!isRight ? (
+        <div className="app-sidebar__head">
+          <div className="app-sidebar__head-inner">
+            <div className="app-sidebar__head-logo">
+              <img src={Logo} alt="Logo" draggable={false} />
+            </div>
+            <button
+              className="app-sidebar__head-btn align-center"
+              onClick={() => toggleSidebar()}
+            >
+              <AppIcon
+                name="icon-menu"
+                width="24"
+                height="24"
+                color="rgba(0, 0, 0, 0.87)"
+              />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="app-sidebar-right__head">
+          <button
+            className="app-sidebar-right__head-btn"
+            onClick={() => toggleSidebar()}
+          >
+            <div
+              className={`app-sidebar-right__head-btn-icon align-center ${
+                isShort && "app-sidebar-right__head-btn-icon_flipped"
+              }`}
+            >
+              <AppIcon
+                name="icon-chevron-up"
+                width="24"
+                height="24"
+                color="rgba(0, 0, 0, 0.87)"
+              />
+            </div>
+            <AppText
+              text="Мой курс приёма"
+              fontSize={22}
+              lineHeight={26}
+              fontWeight={500}
+            />
+          </button>
+          {!isShort && (
+            <div className="app-sidebar-right__head-params">
+              <AppSelect
+                options={selectOptions}
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+              />
+              <button
+                className="head-params__btn"
+                onClick={() => alert("Свернуть все")}
+              >
+                <AppText text="Свернуть все" lineHeight={24} color="#2662C9" />
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+      <div className="app-sidebar__body">{children}</div>
+    </aside>
+  )
+}
 
-export default AppSidebar;
+export default AppSidebar
--- src/components/data-table/DataTable.jsx
+++ src/components/data-table/DataTable.jsx
@@ -1,126 +1,135 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { sortData } from '../../store/LocalDataReducer';
-import { openModalAction } from '../../store/ModalReducer';
-import AppButton from '../app-button/AppButton';
-import { AppIcon } from '../app-icon/AppIcon';
-import AppText from '../app-text/AppText';
-import "./index.scss";
+import React, { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { sortData } from "../../store/LocalDataReducer"
+import { openModalAction } from "../../store/ModalReducer"
+import AppButton from "../app-button/AppButton"
+import { AppIcon } from "../app-icon/AppIcon"
+import AppText from "../app-text/AppText"
+import "./index.scss"
 
 const DataTable = () => {
-	const dispatch = useDispatch()
-	const data = useSelector(state => state.fromStore.data)
-	let sortingArray = [...data]
-	let [sortableTitle, setSortableTitle] = useState([
-		{key: "GoodsCommercialName", direction: "ascending", isActive: false},
-		{key: "CurrentPrices", direction: "ascending", isActive: false}
-	])
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.fromStore.data)
+  let sortingArray = [...data]
+  let [sortableTitle, setSortableTitle] = useState([
+    { key: "GoodsCommercialName", direction: "ascending", isActive: false },
+    { key: "CurrentPrices", direction: "ascending", isActive: false },
+  ])
 
-	const handleSort = (field) => {
-		let paramDirection = field.direction === "ascending" ? 1 : -1
-		sortingArray.sort((a, b) => (
-			a[field.key] < b[field.key] ? -paramDirection
-				: a[field.key] > b[field.key] ? paramDirection
-					: 0
-			)
-		)
-		dispatch(sortData(sortingArray))
-		toggleSortDirection(field)
-	}
+  const handleSort = (field) => {
+    let paramDirection = field.direction === "ascending" ? 1 : -1
+    sortingArray.sort((a, b) =>
+      a[field.key] < b[field.key]
+        ? -paramDirection
+        : a[field.key] > b[field.key]
+        ? paramDirection
+        : 0
+    )
+    dispatch(sortData(sortingArray))
+    toggleSortDirection(field)
+  }
 
-	const toggleSortDirection = (sortedObj) => {
-		let copySortedArray = Object.assign([], sortableTitle)
-		copySortedArray.find(item => {
-			item.isActive = false
-			if (item === sortedObj) {
-				item.isActive = true
-				item.direction === "ascending" ?
-					item.direction = "descending"
-					: item.direction = "ascending"
-			}
-		})
-		setSortableTitle([...copySortedArray])
-	}
+  const toggleSortDirection = (sortedObj) => {
+    let copySortedArray = Object.assign([], sortableTitle)
+    copySortedArray.find((item) => {
+      item.isActive = false
+      if (item === sortedObj) {
+        item.isActive = true
+        item.direction === "ascending"
+          ? (item.direction = "descending")
+          : (item.direction = "ascending")
+      }
+    })
+    setSortableTitle([...copySortedArray])
+  }
 
-	const isAccendingNameClass = sortableTitle[0].direction === "ascending" ?
-		"table-th__sortable_asc"
-		: "table-th__sortable_desc"
-	const isAccendingPriceClass = sortableTitle[1].direction === "ascending" ?
-		"table-th__sortable_asc" 
-		: "table-th__sortable_desc"
+  const isAccendingNameClass =
+    sortableTitle[0].direction === "ascending"
+      ? "table-th__sortable_asc"
+      : "table-th__sortable_desc"
+  const isAccendingPriceClass =
+    sortableTitle[1].direction === "ascending"
+      ? "table-th__sortable_asc"
+      : "table-th__sortable_desc"
 
-	return (
-		<div className="table-wrap">
-			<table className="table">
-				<thead>
-					<tr>
-						<th
-							className={`table-th__sortable ${isAccendingNameClass} `}
-							onClick={() => handleSort(sortableTitle[0])}
-						>
-							<AppText text="Биодобавка" lineHeight={19} />
-							<AppIcon 
-								className="app-icon"
-								name="icon-sort" 
-								width="24" 
-								height="24" 
-								color={sortableTitle[0].isActive ? "#2662C9" : "rgba(0, 0, 0, 0.16)"}
-							/>
-						</th>
-						<th>
-							<AppText text="Описание" lineHeight={19} />
-						</th>
-						<th
-							className={`table-th__sortable ${isAccendingPriceClass} `}
-							onClick={() => handleSort(sortableTitle[1])}
-						>
-							<AppText text="Цена за шт., ₽" lineHeight={19} />
-							<AppIcon 
-								name="icon-sort" 
-								width="24" 
-								height="24" 
-								color={sortableTitle[1].isActive ? "#2662C9" : "rgba(0, 0, 0, 0.16)"}
-							/>
-						</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{data?.map((item) => (
-						<tr key={item?.GoodsCommercialName}>
-							<td className="table__product">
-								<div className="table__product-img align-center">
-									<img src={item?.Picture} />
-								</div>
-								<AppText
-									text={item?.GoodsCommercialName}
-									lineHeight={24}
-								/>
-							</td>
-							<td className="table__description">
-								<AppText 
-									text={item?.CommercialDescription}
-									fontSize={14}
-									lineHeight={18}
-									color={"rgba(0, 0, 0, 0.64)"}
-								/>
-							</td>
-							<td className="table__price">
-								<AppText
-									text={`${item?.CurrentPrices} ₽`}
-									lineHeight={24}
-									fontWeight={700}
-								/>
-							</td>
-							<td>
-								<AppButton text={"Добавить"} onClick={() => dispatch(openModalAction(item))} />
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
-};
+  return (
+    // <div style={{ height: "100vh", overflow: "hidden" }}>
+    <div className="table-wrap">
+      <table className="table">
+        <thead>
+          <tr>
+            <th
+              className={`table-th__sortable ${isAccendingNameClass} `}
+              onClick={() => handleSort(sortableTitle[0])}
+            >
+              <AppText text="Биодобавка" lineHeight={19} />
+              <AppIcon
+                className="app-icon"
+                name="icon-sort"
+                width="24"
+                height="24"
+                color={
+                  sortableTitle[0].isActive ? "#2662C9" : "rgba(0, 0, 0, 0.16)"
+                }
+              />
+            </th>
+            <th>
+              <AppText text="Описание" lineHeight={19} />
+            </th>
+            <th
+              className={`table-th__sortable ${isAccendingPriceClass} `}
+              onClick={() => handleSort(sortableTitle[1])}
+            >
+              <AppText text="Цена за шт., ₽" lineHeight={19} />
+              <AppIcon
+                name="icon-sort"
+                width="24"
+                height="24"
+                color={
+                  sortableTitle[1].isActive ? "#2662C9" : "rgba(0, 0, 0, 0.16)"
+                }
+              />
+            </th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item) => (
+            <tr key={item?.GoodsCommercialName}>
+              <td className="table__product">
+                <div className="table__product-img align-center">
+                  <img src={item?.Picture} />
+                </div>
+                <AppText text={item?.GoodsCommercialName} lineHeight={24} />
+              </td>
+              <td className="table__description">
+                <AppText
+                  text={item?.CommercialDescription}
+                  fontSize={14}
+                  lineHeight={18}
+                  color={"rgba(0, 0, 0, 0.64)"}
+                />
+              </td>
+              <td className="table__price">
+                <AppText
+                  text={`${item?.CurrentPrices} ₽`}
+                  lineHeight={24}
+                  fontWeight={700}
+                />
+              </td>
+              <td>
+                <AppButton
+                  text={"Добавить"}
+                  onClick={() => dispatch(openModalAction(item))}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    // </div>
+  )
+}
 
-export default DataTable;
+export default DataTable
