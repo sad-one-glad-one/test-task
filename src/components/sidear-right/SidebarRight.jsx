890bae102636d731aa3bdc08c5870e4cf77cb1da
sad-one-glad-one <deuslovult17@gmail.com>
2022-11-04 14:05:56 +0700

    separated RightSidebar

---
+++ src/components/sidear-right/SidebarRight.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react"
+import { useSelector } from "react-redux"
+import CollapseListTime from "../../components/collapse-list-time/CollapseListTime"
+import CollapseListBio from "../../components/collapse-list-bio/CollapseListBio"
+import AppSidebar from "../../ui/app-sidebar/AppSidebar"
+import AppTabs from "../../ui/app-tabs/AppTabs"
+import AppText from "../../ui/app-text/AppText"
+
+const SidebarRight = () => {
+  const basketData = useSelector((state) => state.basket.basketData)
+
+  const [isBasketShort, setIsBasketShort] = useState(false)
+  const [activeTab, setActiveTab] = useState(1)
+  const tabs = [
+    { index: 1, text: "По времени приёма" },
+    { index: 2, text: "По биодобавке" },
+  ]
+
+  return (
+    <AppSidebar
+      isShort={isBasketShort}
+      setIsShort={setIsBasketShort}
+      isRight={true}
+    >
+      <div
+        className={isBasketShort ? "basketbar-body_short" : "basketbar-body"}
+      >
+        {isBasketShort && basketData.length > 0 ? (
+          basketData?.map((item, i) => (
+            <div className="basketbar-body__item" key={i}>
+              <div className="align-center img-wrap">
+                <img
+                  src={item.item?.Picture}
+                  alt="mini image"
+                  draggable={false}
+                />
+              </div>
+              <AppText text={item.item?.GoodsCommercialName} />
+            </div>
+          ))
+        ) : isBasketShort && basketData.length < 1 ? (
+          <div className="basketbar-body_short-empty">
+            <AppText text="Выберите биодобавку, чтобы собрать свой персональный курс" />
+          </div>
+        ) : !isBasketShort ? (
+          <div className="basketbar-body">
+            <AppTabs
+              tabs={tabs}
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+            />
+            <div className="basketbar-body__list">
+              {/* ПО ВРЕМЕНИ ПРИЁМА */}
+              {activeTab === 1 && <CollapseListTime />}
+              {/* ПО БИОДОБАВКЕ */}
+              {activeTab === 2 && <CollapseListBio />}
+            </div>
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    </AppSidebar>
+  )
+}
+
+export default SidebarRight
--- src/views/SupplementsView.jsx
+++ src/views/SupplementsView.jsx
@@ -1,27 +1,14 @@
 import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import AppLoader from "../ui/app-loader/AppLoader"
-import AppSidebar from "../ui/app-sidebar/AppSidebar"
-import AppTabs from "../ui/app-tabs/AppTabs"
-import AppText from "../ui/app-text/AppText"
 import DataTable from "../components/data-table/DataTable"
-import CollapseListBio from "../components/collapse-list-bio/CollapseListBio"
-import CollapseListTime from "../components/collapse-list-time/CollapseListTime"
 import AppModal from "../ui/app-modal/AppModal"
-import "./index.scss"
 import SidebarLeft from "../components/sidebar-left/SidebarLeft"
+import SidebarRight from "../components/sidear-right/SidebarRight"
+import "./index.scss"
 
 const SupplementsViews = () => {
   const isLoading = useSelector((state) => state.fromApi.isLoading)
-  const basketData = useSelector((state) => state.basket.basketData)
-
-  const [isBasketShort, setIsBasketShort] = useState(false)
-
-  const [activeTab, setActiveTab] = useState(1)
-  const tabs = [
-    { index: 1, text: "По времени приёма" },
-    { index: 2, text: "По биодобавке" },
-  ]
 
   return (
     <div>
@@ -40,52 +27,7 @@ const SupplementsViews = () => {
         >
           <SidebarLeft />
           <DataTable />
-          <AppSidebar
-            isShort={isBasketShort}
-            setIsShort={setIsBasketShort}
-            isRight={true}
-          >
-            <div
-              className={
-                isBasketShort ? "basketbar-body_short" : "basketbar-body"
-              }
-            >
-              {isBasketShort && basketData.length > 0 ? (
-                basketData?.map((item, i) => (
-                  <div className="basketbar-body__item" key={i}>
-                    <div className="align-center img-wrap">
-                      <img
-                        src={item.item?.Picture}
-                        alt="mini image"
-                        draggable={false}
-                      />
-                    </div>
-                    <AppText text={item.item?.GoodsCommercialName} />
-                  </div>
-                ))
-              ) : isBasketShort && basketData.length < 1 ? (
-                <div className="basketbar-body_short-empty">
-                  <AppText text="Выберите биодобавку, чтобы собрать свой персональный курс" />
-                </div>
-              ) : !isBasketShort ? (
-                <div className="basketbar-body">
-                  <AppTabs
-                    tabs={tabs}
-                    activeTab={activeTab}
-                    setActiveTab={setActiveTab}
-                  />
-                  <div className="basketbar-body__list">
-                    {/* ПО ВРЕМЕНИ ПРИЁМА */}
-                    {activeTab === 1 && <CollapseListTime />}
-                    {/* ПО БИОДОБАВКЕ */}
-                    {activeTab === 2 && <CollapseListBio />}
-                  </div>
-                </div>
-              ) : (
-                <div />
-              )}
-            </div>
-          </AppSidebar>
+          <SidebarRight />
           <AppModal />
         </div>
       )}
